8ebffdfe251a9a70996cfbedf6871fab6dd5e58a
taiyou kuwae <n23013@std.it-college.ac.jp>
2023-12-14 05:55:07 +0000

    2.5 p54,55 finished
    Layoutコンポーネントを作成, HomeコンポーネントでLayoutコンポーネントを使用

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+import Header from 'components/header'
+import Footer from 'components/footer'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
