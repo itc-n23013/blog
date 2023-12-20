287fa43c73200341cf54c87b73f5d4fd2001d3d7
taiyou kuwae <n23013@std.it-college.ac.jp>
2023-12-20 07:34:20 +0000

    修正

--- components/two-column.js
+++ components/two-column.js
@@ -1,27 +1,15 @@
 import styles from 'styles/two-column.module.css'
 
 const TwoColumn = ({ children }) => {
-  return (
-    <div className={styles.flexContainer}>
-      {children}
-    </div>
-  )
+  return <div className={styles.flexContainer}>{children}</div>
 }
 
 const TwoColumnMain = ({ children }) => {
-  return (
-    <div className={styles.main}>
-      {children}
-    </div>
-  )
+  return <div className={styles.main}>{children}</div>
 }
 
 const TwoColumnSidebar = ({ children }) => {
-  return (
-    <div className={styles.sidebar}>
-      {children}
-    </div>
-  )
+  return <div className={styles.sidebar}>{children}</div>
 }
 
-export default {TwoColumn, TwoColumnMain, TwoColumnSidebar}
+export { TwoColumn, TwoColumnMain, TwoColumnSidebar }
