b1dbceac1512126ce13cf6298af953596eda8cb7
taiyou kuwae <n23013@std.it-college.ac.jp>
2024-01-10 07:44:29 +0000

    h

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,22 +1,33 @@
+<<<<<<< HEAD
 import { client } from 'lib/api'
 
 export default function Schedule () {
   return <h1>記事のタイトル</h1>
+=======
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
+
+const Schedule = ({ title, publish, content, eyecatch, categories }) => {
+  return (
+    <Container>
+      <h1>記事のタイトル</h1>
+    </Container>
+  )
+>>>>>>> chapter8
 }
+export default Schedule
 
 export async function getStaticProps () {
-  const resPromise = client.get({
-    endpoint: 'blogs'
-  })
+  const slug = 'schedule'
 
-  try {
-    const res = await resPromise
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
-
+  const post = await getPostBySlug(slug)
   return {
-    props: {}
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories
+    }
   }
 }
