7a00097c69000823ade6c9e1d1042a8581fc64b2
taiyou kuwae <n23013@std.it-college.ac.jp>
2024-01-10 07:28:38 +0000

    sdf

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,18 +1,18 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
-import PostHeader from 'components/post-header'
+
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
     <Container>
-      <article>
-        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
-      </article>
+      <h1>記事のタイトル</h1>
     </Container>
   )
 }
 export default Schedule
+
 export async function getStaticProps () {
   const slug = 'schedule'
+
   const post = await getPostBySlug(slug)
   return {
     props: {
